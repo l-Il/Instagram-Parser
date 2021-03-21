80db2f11b64a51dd57d6fc9166e45bff6eb5b31e
l-Il <0rky@pm.me>
2021-03-21 20:57:25 +0300

    + Similar between two persons added

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ class Parser:
     def __init__(self):
         self.L = Instaloader()
         self.username = ''
+        self.second_username = ''
+        self.first_username = ''
         self.my_login = ''
         self.my_password = ''
-        self.subscribers = []
-        self.subscribed = []
-        self.similar = []
+        self.list1 = []
+        self.list2 = []
+        self.list3 = []
 
         self.root = Tk()
         self.root.title('Instagram Parser')
         self.root.iconbitmap('favicon.ico')
-        self.root.geometry('450x240')
+        self.root.geometry('450x70')
         self.root.resizable(False, True)
         self.root.config(bg='#202020')
 
@@ -24,7 +26,9 @@ class Parser:
         self.my_password_entry = Entry(self.registration_frame, bg='#202020', fg='#FFFFFF', show='*')
         self.status = Label(self.root, text='Можно посмотреть только открытые профили', bg='#202020', fg='#CCCCCC')
         self.username_entry = Entry(self.root, bg='#202020', fg='#CCCCCC')
+        self.second_username_entry = Entry(self.root, bg='#202020', fg='#CCCCCC')
         self.count_similar = Label(self.root, text='Взаимные:', bg='#202020', fg='#CCCCCC')
+
         self.subscribers_label = Label(self.root, text='')
         self.subscribed_label = Label(self.root, text='')
         self.similar_label = Label(self.root, text='')
@@ -37,23 +41,22 @@ class Parser:
         self.status.place(x=150, y=0, width=300)
         Label(self.root, text='Ник:', bg='#202020', fg='#CCCCCC').place(x=150, y=20, height=20)
         self.username_entry.place(x=181, y=20, width=100, height=20)
+        Label(self.root, text='Ник:', bg='#202020', fg='#CCCCCC').place(x=281, y=20, height=20)
+        self.second_username_entry.place(x=315, y=20, width=100, height=20)
         Button(self.root, text='Посмотреть взаимных', bg='#202020', fg='#FF0000', relief=FLAT, border='0', command=self.subs).place(x=150, y=41, height=24)
-        Button(self.root, text='Скачать истории', bg='#202020', fg='#FF00FF', relief=FLAT, border='0', command=self.stories).place(x=280, y=41, height=24)
-        Label(self.root, text='Подписчики:', bg='#202020', fg='#CCCCCC').place(x=0, y=65, height=20)
-        Label(self.root, text='Подписки:', bg='#202020', fg='#CCCCCC').place(x=150, y=65, height=20)
-        self.count_similar.place(x=300, y=65)
-        self.subscribers_label.place(x=0, y=85, width=149, height=150)
-        self.subscribed_label.place(x=150, y=85, width=149, height=150)
-        self.similar_label.place(x=300, y=85, width=149, height=150)
+        Button(self.root, text='Скачать истории', bg='#202020', fg='#FF00FF', relief=FLAT, border='0', command=self.stories).place(x=285, y=41, height=24)
+        Button(self.root, text='Общие', bg='#202020', fg='#FFFF00', relief=FLAT, border='0', command=self.find_similar).place(x=390, y=41, height=24)
+
         self.root.mainloop()
 
     def subs(self):
         try:
+            Label(self.root, text='Подписчики:', bg='#202020', fg='#CCCCCC').place(x=0, y=65, height=20)
+            Label(self.root, text='Подписки:', bg='#202020', fg='#CCCCCC').place(x=150, y=65, height=20)
+            self.count_similar = Label(self.root, text='Взаимные:', bg='#202020', fg='#CCCCCC')
+            self.count_similar.place(x=300, y=65)
             self.status['text'] = 'Загрузка. Подождите'
             self.root.geometry('450x700')
-            self.subscribers_label.place_forget()
-            self.subscribed_label.place_forget()
-            self.similar_label.place_forget()
             self.subscribers_label['text'] = ''
             self.subscribed_label['text'] = ''
             self.similar_label['text'] = ''
@@ -68,18 +71,18 @@ class Parser:
             profile = Profile.from_username(self.L.context, self.username)
 
             for i in profile.get_followees():
-                self.subscribed.append(i.username)
+                self.list2.append(i.username)
                 self.subscribed_label['text'] = self.subscribed_label['text'] + '\n' + str(i.username)
 
             for i in profile.get_followers():
-                self.subscribers.append(i.username)
+                self.list1.append(i.username)
                 self.subscribers_label['text'] = self.subscribers_label['text'] + '\n' + str(i.username)
 
-            for i in range(len(self.subscribers)):
-                if self.subscribers[i] in self.subscribed:
-                    self.similar.append(self.subscribers[i])
-                    self.similar_label['text'] = self.similar_label['text'] + '\n' + str(self.subscribers[i])
-                    self.count_similar['text'] = 'Взаимные: ' + str(len(self.similar))
+            for i in range(len(self.list1)):
+                if self.list1[i] in self.list2:
+                    self.list3.append(self.list1[i])
+                    self.similar_label['text'] = self.similar_label['text'] + '\n' + str(self.list1[i])
+                    self.count_similar['text'] = 'Взаимные: ' + str(len(self.list3))
             self.status['text'] = 'Готово!'
         except Exception as e:
             self.status['text'] = e
@@ -89,9 +92,41 @@ class Parser:
             self.status['text'] = 'Загрузка. Подождите'
             self.root.update()
             self.username = self.username_entry.get()
-            profile = self.L.check_profile_id(self.username)
+            self.my_login = self.my_login_entry.get()
+            self.my_password = self.my_password_entry.get()
+            self.L.login(self.my_login, self.my_password)
+            profile = Profile.from_username(self.L.context, self.username)
             print(profile.userid)
             self.L.download_stories(userids=[profile.userid])
+            self.status['text'] = 'Готово!'
+        except Exception as e:
+            self.status['text'] = e
+
+    def find_similar(self):
+        try:
+            self.status['text'] = 'Загрузка. Подождите'
+            self.root.geometry('450x700')
+            self.subscribed_label['text'] = ''
+            self.subscribed_label.place(x=150, y=85, width=149, height=610)
+            self.root.update()
+            self.first_username = self.username_entry.get()
+            self.second_username = self.second_username_entry.get()
+            self.my_login = self.my_login_entry.get()
+            self.my_password = self.my_password_entry.get()
+
+            self.L.login(self.my_login, self.my_password)
+            profile1 = Profile.from_username(self.L.context, self.first_username)
+            profile2 = Profile.from_username(self.L.context, self.second_username)
+
+            for i in profile1.get_followees():
+                self.list1.append(i.username)
+            for i in profile2.get_followees():
+                self.list2.append(i.username)
+
+            for i in range(len(self.list1)):
+                if self.list1[i] in self.list2:
+                    self.list3.append(self.list1[i])
+                    self.subscribed_label['text'] = self.subscribed_label['text'] + '\n' + str(self.list1[i])
             self.status['text'] = 'Готово!'
         except Exception as e:
             self.status['text'] = e
